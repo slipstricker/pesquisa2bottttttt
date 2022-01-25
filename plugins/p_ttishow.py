b544a0b8ec3443bc79286b69bf63831e3fb3499d
slipstricker <48421749+slipstricker@users.noreply.github.com>
2022-01-24 22:14:39 -0300

    Update p_ttishow.py

--- plugins/p_ttishow.py
+++ plugins/p_ttishow.py
@@ -37,8 +37,7 @@ async def save_group(bot, message):
             await bot.leave_chat(message.chat.id)
             return
         buttons = [[
-            InlineKeyboardButton('ℹ️ Help', url=f"https://t.me/{temp.U_NAME}?start=help"),
-            InlineKeyboardButton('📢 Updates', url='https://t.me/TMWAD')
+            InlineKeyboardButton('📢 Canal oficial', url='https://t.me/@ModGames')
         ]]
         reply_markup=InlineKeyboardMarkup(buttons)
         await message.reply_text(
